bcec5b16612554c4cbea65a26d9d87afe497fdd0
Daniela Barbosa <contato@danielacb.com>
2021-08-23 01:18:32 -0300

    create data scraper

--- api/game/controllers/game.js
+++ api/game/controllers/game.js
@@ -1,8 +1,22 @@
-'use strict';
+"use strict";
 
 /**
  * Read the documentation (https://strapi.io/documentation/developer-docs/latest/development/backend-customization.html#core-controllers)
  * to customize this controller
  */
 
-module.exports = {};
+module.exports = {
+  populate: async (ctx) => {
+    console.log("Starting to populate...");
+
+    const options = {
+      sort: "popularity",
+      page: "2",
+      ...ctx.query,
+    };
+
+    await strapi.services.game.populate(options);
+
+    ctx.send("Finished populating");
+  },
+};
