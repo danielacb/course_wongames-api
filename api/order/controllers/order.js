9e940d6acf5dd8484d8e5b22abf09ca6596dc0ea
Daniela Barbosa <contato@danielacb.com>
2022-08-25 00:29:34 -0300

    add createPaymentIntent controller to Order

--- api/order/controllers/order.js
+++ api/order/controllers/order.js
@@ -1,8 +1,53 @@
-'use strict';
+"use strict";
 
-/**
- * Read the documentation (https://strapi.io/documentation/developer-docs/latest/development/backend-customization.html#core-controllers)
- * to customize this controller
- */
+const stripe = require("stripe")(process.env.STRIPE_KEY);
 
-module.exports = {};
+module.exports = {
+  createPaymentIntent: async (ctx) => {
+    const { cart } = ctx.request.body;
+    const games = [];
+
+    await Promise.all(
+      cart?.map(async (game) => {
+        const validatedGame = await strapi.services.game.findOne({
+          id: game.id,
+        });
+
+        if (validatedGame) games.push(validatedGame);
+      })
+    );
+
+    if (!games.length) {
+      ctx.response.status = 404;
+      return {
+        error: "No valid games found!",
+      };
+    }
+
+    const total = games.reduce((acc, game) => {
+      return acc + game.price;
+    }, 0);
+
+    if (total === 0) {
+      return {
+        freeGames: true,
+      };
+    }
+
+    try {
+      const paymentIntent = await stripe.paymentIntents.create({
+        amount: Math.round(total * 100),
+        currency: "usd",
+        metadata: { integration_check: "accept_a_payment" },
+      });
+
+      return paymentIntent;
+    } catch (err) {
+      return {
+        error: err.raw.message,
+      };
+    }
+
+    return { total_in_cents: total * 100, games };
+  },
+};
