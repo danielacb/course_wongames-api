712676af4643eab935260a2ea4e699e607292a02
Daniela Barbosa <contato@danielacb.com>
2022-12-08 23:46:59 -0300

    setup emails with sendinblue

--- config/env/production/plugins.js
+++ config/env/production/plugins.js
@@ -10,5 +10,20 @@ module.exports = ({ env }) => ({
       upload: {},
       delete: {},
     },
+    email: {
+      provider: "nodemailer",
+      providerOptions: {
+        host: env("SMTP_HOST", "smtp-relay.sendinblue.com"),
+        port: env("SMTP_PORT", 587),
+        auth: {
+          user: env("SMTP_USERNAME"),
+          pass: env("SMTP_PASSWORD"),
+        },
+      },
+      settings: {
+        defaultFrom: "contact@wongames.com",
+        defaultReplyTo: "contact@wongames.com",
+      },
+    },
   },
 });
